ab616e5b3051caa1b1434836b48493fc40a68c0b
CarlosCE-dev <c4r10z.n3t@gmail.com>
2021-05-22 13:12:37 -0600

    Updates

--- _nuxt/static/1621709670/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FEdenProjectWeb\u002F_nuxt\u002Fstatic\u002F1621709670",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FEdenProjectWeb\u002F",assetsPath:"\u002FEdenProjectWeb\u002F_nuxt\u002F",cdnURL:a}}}}(null));
